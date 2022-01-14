c380bb8dbf5dbb32b32666b73f76b47f67343421
Chungyeonsoo <80508732+Chungyeonsoo@users.noreply.github.com>
2022-01-15 01:17:08 +0900

    Update BTCAutoTrade.py

--- BTCAutoTrade.py
+++ BTCAutoTrade.py
@@ -46,6 +46,12 @@ def get_current_price(ticker):
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
+btc_total = float(upbit.get_balances()[1]['balance']) * float(upbit.get_balances()[1]['avg_buy_price'])
+doge_total = float(upbit.get_balances()[2]['balance']) * float(upbit.get_balances()[2]['avg_buy_price'])
+balance_sum = btc_total + doge_total + float(upbit.get_balances()[0]['balance'])
+                                     
+btc_per = btc_total / balance_sum
+doge_per = doge_total / balance_sum
 
 # 자동매매 시작
 while True:
@@ -56,24 +62,38 @@ while True:
         end_time = start_time + datetime.timedelta(hours=4)
 
         if start_time < now < (end_time - datetime.timedelta(seconds=10)):
-            target_price = get_target_price("KRW-BTC", 0.005)
+            target_price = get_target_price("KRW-BTC", 0.01)
             current_price = get_current_price("KRW-BTC")
             limit_price = get_limit_price("KRW-BTC", 0.5)
             
-            target_price_DOGE = get_target_price("KRW-DOGE", 0.005)
-            current_price = get_current_price("KRW-DOGE")
-            limit_price = get_limit_price("KRW-DOGE", 0.5)
+            target_price_DOGE = get_target_price("KRW-DOGE", 0.01)
+            current_price_DOGE = get_current_price("KRW-DOGE")
+            limit_price_DOGE = get_limit_price("KRW-DOGE", 0.5)
             
-            if target_price < current_price < limit_price:
-                krw = get_balance("KRW")
-                dist_krw = (krw * 0.5) - 100
-                left_krw = krw - dist_krw - 100
-                if dist_krw > 5000:
-                    upbit.buy_market_order("KRW-BTC", dist_krw*0.9995)
+            if target_price < current_price < limit_price and btc_per < 0.49:
+                amount_krw = balance_sum * 0.49 - btc_total
+                if amount_krw > 5000:
+                    upbit.buy_market_order("KRW-BTC", amount_krw*0.9995)
                     print('buy BTC')
-                if left_krw > 5000:
-                    upbit.buy_market_order("KRW-DOGE", left_krw*0.9995)
+                    btc_total = float(upbit.get_balances()[1]['balance']) * float(upbit.get_balances()[1]['avg_buy_price'])
+                    doge_total = float(upbit.get_balances()[2]['balance']) * float(upbit.get_balances()[2]['avg_buy_price'])
+                    balance_sum = btc_total + doge_total + float(upbit.get_balances()[0]['balance'])
+                                     
+                    btc_per = btc_total / balance_sum
+                    doge_per = doge_total / balance_sum
+                    
+            if target_price_DOGE < current_price_DOGE < limit_price_DOGE and doge_per < 0.49:
+                amount_krw = balance_sum * 0.49 - doge_total
+                if amount_krw > 5000:
+                    upbit.buy_market_order("KRW-DOGE", amount_krw*0.9995)
                     print('buy DOGE')
+                    btc_total = float(upbit.get_balances()[1]['balance']) * float(upbit.get_balances()[1]['avg_buy_price'])
+                    doge_total = float(upbit.get_balances()[2]['balance']) * float(upbit.get_balances()[2]['avg_buy_price'])
+                    balance_sum = btc_total + doge_total + float(upbit.get_balances()[0]['balance'])
+                                     
+                    btc_per = btc_total / balance_sum
+                    doge_per = doge_total / balance_sum
+
                     
         else:
             btc = get_balance("BTC")
@@ -89,4 +109,4 @@ while True:
         time.sleep(1)
     except Exception as e:
         print(e)
-        time.sleep(1)    
+        time.sleep(1)  
